c127431b54745eac7d6b2e80607cb72b813166a0
Joe Hinkley <joehhinkley@gmail.com>
2018-05-08 15:05:10 -0700

    Change mobile and desktop nav

--- MilestoneFrontend/src/components/Notification.js
+++ MilestoneFrontend/src/components/Notification.js
@@ -35,7 +35,7 @@ class Notification extends React.Component {
         var notifications;
         if (this.state && this.state.user) {
             var count = 0;
-            notifications = this.state.user.notifications.map((notification) => {
+            notifications = (this.props.isDropdown ? this.state.user.notifications.slice(0, 5) : this.state.user.notifications).map((notification) => {
                 var body;
                 var time;
                 var read;
@@ -89,7 +89,7 @@ class Notification extends React.Component {
         }
         return (
             <div className="l-notifications">
-                <h1 className="c-notifications-header">Notifications</h1>
+                <h1 className={this.props.isDropdown ? "c-notifications-dropdown-header" : "c-notifications-header"}>Notifications</h1>
                 {notifications}
             </div>
         );
