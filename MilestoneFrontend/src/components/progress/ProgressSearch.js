6f70e5b174759e75865786d2c84dbeb1eb470473
Iean Drew <ieand@uw.edu>
2018-05-09 02:37:14 -0700

    Fixed 'pressing enter to search' in Progress

--- MilestoneFrontend/src/components/progress/ProgressSearch.js
+++ MilestoneFrontend/src/components/progress/ProgressSearch.js
@@ -19,9 +19,9 @@ import '../../css/progress/ProgressSearch.css';
 function ProgressSearch(props) {
     return (
         <div className="c-progress-form">
-            <form className="[ form-inline ] c-progress-search">
+            <form className="[ form-inline ] c-progress-search"  onSubmit={ (e) => props.handleSearch(e) } >
                 <input id="progressSearch" className="form-control mr-sm-2" type="search" placeholder="Search..." aria-label="Search"/>
-                <Button className="btn btn-outline-success my-2 my-sm-0 c-progress-search__search-button" onClick={(e) => props.handleSearch(e)}>
+                <Button className="btn btn-outline-success my-2 my-sm-0 c-progress-search__search-button" onClick={ (e) => props.handleSearch(e) }>
                     <Glyphicon glyph="search" /> 
                 </Button>
                 <Link to={ props.addBtnLink }>
